3d54d3ea6dd3936cf7fff8be2ca62d760484975f
Itzhak Kakon <izakkakon@gmail.com>
2025-08-21 16:37:52 +0300

    .

--- app/manager.py
+++ app/manager.py
@@ -1,20 +1,20 @@
-import pandas as pd
-from processor import DataProcessor
-
-
-class AnalysisManager:
-    def __init__(self, df: pd.DataFrame):
-        self.raw_data_df = df
-        self.processor = DataProcessor()
-        self.processed_data = None
-
-    def run_full_analysis(self):
-        if self.raw_data_df.empty:
-            self.processed_data = []
-            return
-
-        processed_df = self.processor.process_data(self.raw_data_df)
-        self.processed_data = processed_df.to_dict(orient='records')
-
-    def get_processed_data(self):
-        return self.processed_data
+# import pandas as pd
+# from processor import DataProcessor
+#
+#
+# class AnalysisManager:
+#     def __init__(self, df: pd.DataFrame):
+#         self.raw_data_df = df
+#         self.processor = DataProcessor()
+#         self.processed_data = None
+#
+#     def run_full_analysis(self):
+#         if self.raw_data_df.empty:
+#             self.processed_data = []
+#             return
+#
+#         processed_df = self.processor.process_data(self.raw_data_df)
+#         self.processed_data = processed_df.to_dict(orient='records')
+#
+#     def get_processed_data(self):
+#         return self.processed_data
